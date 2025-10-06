66a8f4e27d75b5a631c412bd038b4732d6cee1e6
Karina Duisenbekova <xduise00@vutbr.cz>
2025-10-06 14:31:13 +0200

    Develop a Home tab test case

--- tests/hawtio-test-suite/src/main/java/io/hawt/tests/features/stepdefinitions/panel/PreferencesStepDefs.java
+++ tests/hawtio-test-suite/src/main/java/io/hawt/tests/features/stepdefinitions/panel/PreferencesStepDefs.java
@@ -1,8 +1,15 @@
 package io.hawt.tests.features.stepdefinitions.panel;
 
+import io.cucumber.java.PendingException;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.hawt.tests.features.pageobjects.pages.preferences.PreferencesPage;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.$;
 
 public class PreferencesStepDefs {
     private final PreferencesPage preferencesPage = new PreferencesPage();
@@ -16,4 +23,30 @@ public class PreferencesStepDefs {
     public void theContentOfPreferencesPageIsOpen() {
         preferencesPage.checkContent();
     }
+
+    @When("User toggles the show vertical navigation field")
+    public void userTogglesTheField() {
+        $(By.cssSelector("main span.pf-v5-c-switch__toggle")).shouldBe(interactable).click();
+    }
+
+    @When("User clicks on Reset button")
+    public void userClicksOnButton() {
+         $(By.cssSelector("main button.pf-m-danger")).shouldBe(interactable).click();
+    }
+
+    @And("User confirms resetting the setting of Hawtio")
+    public void userConfirmsResettingTheSettingOfHawtio() {
+        $("[data-testid='reset-settings-modal']")
+            .shouldBe(visible)
+            .shouldHave(text("You are about to reset all the Hawtio settings."));
+        $(By.cssSelector(".pf-v5-c-modal-box__footer")).$("[data-testid='reset-btn']").shouldBe(visible, enabled).click();
+    }
+
+    @Then("User is presented with a successful alert message")
+    public void userIsPresentedWithASuccessfullAlertMessage() {
+        $(By.cssSelector("main .pf-v5-c-alert")).shouldBe(visible);
+    }
+
+
+
 }
--- tests/hawtio-test-suite/src/test/resources/io/hawt/tests/features/panel/preferences/preferences.feature
+++ tests/hawtio-test-suite/src/test/resources/io/hawt/tests/features/panel/preferences/preferences.feature
@@ -22,3 +22,12 @@ Feature: Checking the functionality of a Preferences tab
     Examples:
       |Connect      |
 
+  Scenario: Check that Home tab works
+    Given User clicks on "Preferences" option in hawtio drop-down menu
+    And User is on "Home" tab of Preferences page
+    When User toggles the show vertical navigation field
+    When User clicks on Reset button
+    And User confirms resetting the setting of Hawtio
+    Then User is presented with a successful alert message
+
+
